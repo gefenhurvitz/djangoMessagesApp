d749c6314c83708809b3a63355dc5148e0362e39
gefen <gh.production.isr@gmail.com>
2022-08-14 08:59:50 +0300

    getting ready to deploy

--- backend/settings.py
+++ backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dvy(!#v3s0ufk!0y68*f7vjuwa%sk&7_+1$cj(dnr3)yuabd!y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-messages-app.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
